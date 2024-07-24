110e9027af124f514412ff5144eb2856b497bfbd
merlina-wizard <merlini.genshin@gmail.com>
2024-07-25 00:25:23 +0200

    01

--- Library/Library/src/main/java/com/sistemi/informativi/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sistemi.informativi.library.repository;
-
-import com.sistemi.informativi.library.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
